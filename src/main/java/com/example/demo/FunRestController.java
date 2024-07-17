c11ea41834a498add151933a38d6b17aa2dc5bbb
Miguel Montagner <dev@miguelmontagner.com>
2024-07-17 05:41:17 +0100

    added custom properties

--- src/main/java/com/example/demo/FunRestController.java
+++ src/main/java/com/example/demo/FunRestController.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject some custom properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
     @GetMapping("/")
     public String sayHello() {
         return "hello!";
@@ -15,5 +22,9 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is your lucky day.";
+    }
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + " | Team Name: " + teamName;
     }
 }
